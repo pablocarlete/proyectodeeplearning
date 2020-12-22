2853c8bf8bb88a40dc80e374dc3caec9c75ccec6
pablocarlete <50927984+pablocarlete@users.noreply.github.com>
2020-12-22 13:55:57 -0300

    Add files via upload

--- utils/_tools.py
+++ utils/_tools.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-def split_dataframe(df, train=0.7, val=0.2):
+def split_dataframe(df, train=0.7, val=0.2, dis=False):
 	"""
 	divide un dataframe en tres partes.
 	
@@ -15,6 +15,9 @@ def split_dataframe(df, train=0.7, val=0.2):
 	:param float val:
 		proporción de datos de validación
 		
+	:param boolean dis:
+		Si es True se imprime la cantidad de datos en cada dataframe
+		
 	return:
 	df_train, df_val, df_test
 	"""
@@ -22,5 +25,10 @@ def split_dataframe(df, train=0.7, val=0.2):
 	indx_2 = int( (train+val)*df.shape[0])
 	
 	df_train, df_val, df_test = df.iloc[:indx_1, :], df.iloc[indx_1:indx_2, :], df.iloc[indx_2:, :]
+	
+	if dis:
+		print( 'train split: {:d} data points'.format(df_train.shape[0]) )
+		print( 'validation split: {:d} data points'.format(df_val.shape[0]) )
+		print( 'test split: {:d} data points'.format(df_test.shape[0]) )
 	
 	return df_train, df_val, df_test
