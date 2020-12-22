f8e1e894d0d04419bfffd2b1e4e21f00512ed94c
pablocarlete <50927984+pablocarlete@users.noreply.github.com>
2020-12-22 13:45:19 -0300

    Add files via upload

--- utils/_tools.py
+++ utils/_tools.py
@@ -1,6 +1,26 @@
-def ola(a, b):
+import panda as pd
+import numpy as np
+
+
+def split_dataframe(df, train=0.7, val=0.2):
 	"""
-	ola, estoi solo estoy probando como hacer funciones impotables en github jejeje
+	divide un dataframe en tres partes.
 	
+	:param pd.dataframe df:
+		panda dataframe
+	
+	:param float train:
+		proporción de datos de entrenamiento
+		
+	:param float val:
+		proporción de datos de validación
+		
+	return:
+	df_train, df_val, df_test
 	"""
-	return a+b
+	indx_1 = int( train*df.shape[0])
+	indx_2 = int( (train+val)*df.shape[0])
+	
+	df_train, df_val, df_test = df.iloc[:indx1, :], df.iloc[indx1:indx2, :], df.iloc[indx2:, :]
+	
+	return df_train, df_val, df_test
