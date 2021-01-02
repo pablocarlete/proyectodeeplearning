93f95650360a82add6dcb88c9da4ea824f618cf8
pablocarlete <50927984+pablocarlete@users.noreply.github.com>
2021-01-02 19:20:42 -0300

    Add files via upload

--- utils/_tools_2.py
+++ utils/_tools_2.py
@@ -302,3 +302,100 @@ def generate_dataset_sc(df, nperseg, noverlap, train=0.7, val=0.2,
 
 	return X_train, X_val, X_test, Y_train, Y_val, Y_test
 # ----------------------------------------------------------------------------
+def plot_confusion_matrix(Y_true, Y_pred, target_names,
+                          title='Confusion matrix',
+                          cmap=None, normalize=False,
+                          figsize=(5,5)):
+    
+    """
+    given the true (Y_true) and the predicted (Y_pred) labels,
+    makes the confusion matrix.
+    
+    :param np.array Y_true:
+        the true labels of the data. (no one hot encoding).
+    :param np.array Y_pred:
+        the predicted labels of the data by the model. (no one hot encoding).
+    :param list target_names:
+        given classification classes such as [0, 1, 2] the class names,
+        for example: ['high', 'medium', 'low'].
+    :param str title:
+        the text to display at the top of the matrix.
+    :param str cmap:
+        the gradient of the values displayed from matplotlib.pyplot.cm
+        see http://matplotlib.org/examples/color/colormaps_reference.html
+        plt.get_cmap('jet') or plt.cm.Blues.
+    :param bool normalize:
+        if False, plot the raw numbers, if True, plot the proportions.
+        
+    :reference:
+        http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+        
+    """
+    import itertools
+    
+    cm = confusion_matrix(Y_true, Y_pred)
+    
+    accuracy = np.trace(cm) / float(np.sum(cm))
+    misclass = 1 - accuracy
+    
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+    
+    plt.figure(figsize=figsize)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+    
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    
+    
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                      verticalalignment="center",
+                      horizontalalignment="center",
+                      color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                      verticalalignment="center",
+                      horizontalalignment="center",
+                      color="white" if cm[i, j] > thresh else "black")
+    
+    
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    plt.show()
+    
+# ----------------------------------------------------------------------------
+def plot_loss_function(train_info, figsize=(5,5)):
+    """
+    -> None
+    
+    this function plots de evolution of the loss function of the model 
+    during the training epochs.
+    
+    :param train_info:
+        training history of the classification model.
+        
+    """
+    # crear figura
+    plt.figure(figsize=figsize)
+    
+    plt.plot(train_info.history['loss'])
+    plt.plot(train_info.history['val_loss'])
+    
+    # caracteristicas del plot
+    plt.title('Model loss')
+    plt.ylabel('Loss'); plt.xlabel('Epoch')
+    plt.legend(['train', 'validation'], loc='upper right')
+    plt.show()
+ 
+# ----------------------------------------------------------------------------
